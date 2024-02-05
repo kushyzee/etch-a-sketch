31df3f887c498155c7140bb537d722900a1c3d27
Arinze Ogbuniba <niklaus.oa@gmail.com>
2024-02-05 08:56:43 +0100

    Change the logic of calculating flex basis
    the previous calculation gave the wrong size because it divided the div
    container by the number of rows before subtracting border width (16px)

--- script.js
+++ script.js
@@ -6,11 +6,14 @@ const gridContainer = document.querySelector(".container");
 rowNumber.value = 16
 columnNumber.textContent = rowNumber.value;
 
+function removePreviousGrid () {
+  console.log(gridContainer)
+}
+
 function createGrid() {
   let row = parseInt(rowNumber.value);
   let rowAndColumn = row * row
-  let flexBasis = 800 / row - 1
-  console.log(flexBasis)
+  let flexBasis = (800 - 16) / row
   for (let i = 0; i < rowAndColumn; i++) {
     let newBox = document.createElement('div')
     newBox.style.cssText = `flex-basis: ${flexBasis}px`
@@ -19,3 +22,4 @@ function createGrid() {
 }
 
 createGrid()
+enterBtn.addEventListener('click', removePreviousGrid)
