e7b234680050123be0c38038cb779b4b65a8ca62
Arinze Ogbuniba <niklaus.oa@gmail.com>
2024-02-05 09:59:39 +0100

    Create a function that init grid row and column

--- script.js
+++ script.js
@@ -3,33 +3,41 @@ const columnNumber = document.querySelector("#column-number");
 const enterBtn = document.querySelector("button");
 const gridContainer = document.querySelector(".container");
 
-rowNumber.value = 4
-columnNumber.textContent = rowNumber.value;
-
-function formatInput () {
-  rowNumber.value = Math.floor(rowNumber.value)
-  columnNumber.textContent = rowNumber.value
-  if(rowNumber.value === 0) console.log('NaN')
+function setRowAndCol() {
+  rowNumber.value = 4;
+  columnNumber.textContent = rowNumber.value;
 }
 
-function removePreviousGrid () {
-  Array.from(gridContainer.children).forEach(box => box.remove())
-  columnNumber.textContent = rowNumber.value
-  createGrid()
+function formatInput() {
+  rowNumber.value = Math.floor(rowNumber.value);
+  columnNumber.textContent = rowNumber.value;
+}
+
+function removePreviousGrid() {
+  if (rowNumber.value < 1) {
+    alert("Please enter a number between 1 and 100");
+    setRowAndCol()
+    return
+  }
+
+  Array.from(gridContainer.children).forEach((box) => box.remove());
+  columnNumber.textContent = rowNumber.value;
+  createGrid();
 }
 
 function createGrid() {
   let row = parseInt(rowNumber.value);
-  let rowAndColumn = row * row
-  let flexBasis = (800 - 16) / row
+  let rowAndColumn = row * row;
+  let flexBasis = (800 - 16) / row;
   for (let i = 0; i < rowAndColumn; i++) {
-    let newBox = document.createElement('div')
-    newBox.style.cssText = `flex-basis: ${flexBasis}px`
-    gridContainer.appendChild(newBox)
+    let newBox = document.createElement("div");
+    newBox.style.cssText = `flex-basis: ${flexBasis}px`;
+    gridContainer.appendChild(newBox);
   }
 }
 
-createGrid()
+setRowAndCol()
+createGrid();
 
-enterBtn.addEventListener('click', removePreviousGrid)
-rowNumber.addEventListener('input', formatInput)
+enterBtn.addEventListener("click", removePreviousGrid);
+rowNumber.addEventListener("input", formatInput);
